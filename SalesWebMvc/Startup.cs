d8d295b2af233c0fffb217b173b15a65317a1510
Caique Gomes <caiquegomesdev@gmail.com>
2024-03-24 03:17:20 -0300

    SellersController and update SeedingService

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -42,6 +42,7 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
